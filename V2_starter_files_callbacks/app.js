0c46bfeb83685bfe573f1514debc84ec5c8a4676
nfluk <nickylukic@gmail.com>
2022-04-27 08:35:15 +0200

    refactoring to promises setup

--- V2_starter_files_callbacks/app.js
+++ V2_starter_files_callbacks/app.js
@@ -9,22 +9,27 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.static("public"));
 
 //CALL BACKS
-function getUsers(cb) {
-  fs.readFile("data.json", "utf8", (err, data) => {
-    if (err) return cb(err);
-    const users = JSON.parse(data);
-    return cb(null, users);
-  });
-}
+// function getUsers(cb) {
+//   fs.readFile("data.json", "utf8", (err, data) => {
+//     if (err) return cb(err);
+//     const users = JSON.parse(data);
+//     return cb(null, users);
+//   });
+// }
 
-app.get("/", (req, res) => {
-  getUsers((err, users) => {
-    if (err) {
-      res.render("error", { error: err });
-    } else {
-      res.render("index", { title: "Users", users: users.users });
-    }
-  });
-});
+// app.get("/", (req, res) => {
+//   getUsers((err, users) => {
+//     if (err) {
+//       res.render("error", { error: err });
+//     } else {
+//       res.render("index", { title: "Users", users: users.users });
+//     }
+//   });
+// });
+
+//PROMISES
+function getUsers() {}
+
+app.get("/", (req, res) => {});
 
 app.listen(3000, () => console.log("App listening on port 3000!"));
